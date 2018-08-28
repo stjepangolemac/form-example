c8aaefd7014bc2ee63f8bbd428242250f5c3ab2b
Stjepan Golemac <stjepan.golemac@gmail.com>
2018-08-28 09:00:34 +0200

    fix(eject): Eject from CRAP as tests would not work

--- src/__tests__/utils/validators.js
+++ src/__tests__/utils/validators.js
@@ -3,3 +3,7 @@ import { isEmpty } from 'util/validators'
 test('isEmpty returns an error for empty string', () => {
   expect(isEmpty('')).toBeTruthy()
 })
+
+test('isEmpty does not return error for non empty string', () => {
+  expect(isEmpty('random string')).toBeUndefined()
+})
